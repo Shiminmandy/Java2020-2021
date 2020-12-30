6ec07f688ade70abbe024e80cd112debe98c080d
Shiminmandy <mandyshimin18@gmail.com>
2020-12-30 00:56:40 -0500

    2020-2021

---
+++ JavaWinter/src/com/imooc/hw/Employee.java
@@ -0,0 +1,11 @@
+package com.imooc.hw;
+
+public class Employee {
+    String name;
+    public Employee(){
+
+    }
+    public void run(){
+        System.out.println("hello");
+    }
+}
---
+++ JavaWinter/src/com/imooc/hw/Test.java
@@ -0,0 +1,8 @@
+package com.imooc.hw;
+
+public class Test {
+    public static void main(String[] args){
+        Employee e=new Employee();
+        e.run();
+    }
+}
