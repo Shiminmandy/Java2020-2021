adeba5c20a6f7b6bc753d3104d3242c9a62178f1
Shiminmandy <mandyshimin18@gmail.com>
2021-01-12 15:58:54 -0500

    饿汉式”

--- SingletonProj/src/com/imooc/test/SingletonTest.java
+++ SingletonProj/src/com/imooc/test/SingletonTest.java
@@ -1,11 +1,17 @@
 package com.imooc.test;
 
 import com.imooc.singleton.SingletonOne;
+import com.imooc.singleton.SingletonTwo;
 
 public class SingletonTest {
     public static void main(String[] args){
         SingletonOne one=SingletonOne.getInstance();
         SingletonOne two=SingletonOne.getInstance();
         System.out.println(one==two); //true
+        System.out.println("======================");
+        SingletonTwo three= SingletonTwo.getInstance();
+        SingletonTwo four=SingletonTwo.getInstance();
+        System.out.println(three==four);
+
     }
 }
