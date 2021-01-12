0cd189c3243fe5ad33efa5f15185887e12890cd6
Shiminmandy <mandyshimin18@gmail.com>
2021-01-12 15:43:39 -0500

    Singleton

--- SingletonProj/src/com/imooc/test/SingletonTest.java
+++ SingletonProj/src/com/imooc/test/SingletonTest.java
@@ -1,4 +1,11 @@
 package com.imooc.test;
 
+import com.imooc.singleton.SingletonOne;
+
 public class SingletonTest {
+    public static void main(String[] args){
+        SingletonOne one=SingletonOne.getInstance();
+        SingletonOne two=SingletonOne.getInstance();
+        System.out.println(one==two); //true
+    }
 }
