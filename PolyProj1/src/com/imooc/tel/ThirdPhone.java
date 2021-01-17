6628659204ad023325901e39039f23e20ac84b5d
Shiminmandy <mandyshimin18@gmail.com>
2021-01-16 23:21:57 -0500

    poly

---
+++ PolyProj1/src/com/imooc/tel/ThirdPhone.java
@@ -0,0 +1,11 @@
+package com.imooc.tel;
+
+public class ThirdPhone extends SecondPhone{
+    public void vedio(){
+        System.out.println("手机可以。");
+    }
+
+    public void music(){
+        System.out.println("手机可以听音乐。");
+    }
+}
---
+++ PolyProj1/src/com/imooc/test/PhoneTest.java
@@ -0,0 +1,15 @@
+package com.imooc.test;
+
+import com.imooc.tel.FourthPhone;
+
+public class PhoneTest {
+    public static void main(String[] args){
+        FourthPhone phone=new FourthPhone();
+        phone.game();
+        phone.netWork();
+        phone.photo();
+        phone.call();
+        phone.message();
+        phone.music();
+    }
+}
