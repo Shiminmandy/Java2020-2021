364523e4b6a449898c081ead69db305d216e0d9c
Shiminmandy <mandyshimin18@gmail.com>
2021-01-18 22:56:50 -0500

    interface

--- PolyProj1/src/com/imooc/test/PhoneTest.java
+++ PolyProj1/src/com/imooc/test/PhoneTest.java
@@ -1,8 +1,7 @@
 package com.imooc.test;
 
-import com.imooc.tel.FourthPhone;
-import com.imooc.tel.IPhoto;
-import com.imooc.tel.Camera;
+import com.imooc.tel.*;
+
 public class PhoneTest {
     public static void main(String[] args){
         FourthPhone phone=new FourthPhone();
@@ -17,6 +16,12 @@ public class PhoneTest {
         ip.photo();
         ip=new Camera();
         ip.photo();
+        System.out.println("=====================");
+        System.out.println(INet.TEMP);
+        INet net=new SmartWatch();
+        System.out.println(net.TEMP);//20?30?
+        SmartWatch sw=new SmartWatch();
+        System.out.println(sw.TEMP);
 
 
 
