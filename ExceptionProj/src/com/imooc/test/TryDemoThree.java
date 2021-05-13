56a5f43c98ca74d0521b4169a9f43d10bb065027
Shiminmandy <mandyshimin18@gmail.com>
2021-05-13 14:52:13 -0400

    TryDemoTwo

--- ExceptionProj/src/com/imooc/test/TryDemoThree.java
+++ ExceptionProj/src/com/imooc/test/TryDemoThree.java
@@ -1,31 +1,23 @@
 package com.imooc.test;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-public class TryDemoTwo {
+public class TryDemoThree {
     public static void main(String[] args){
         int result =test();
         System.out.println("one和two的商是："+result);
     }
 
-    public static int test(){
+    public static int test() throws ArithmeticException{
         Scanner input=new Scanner(System.in);
         System.out.println("=====运算开始=====");
-        try{
+
             System.out.print("请输入第一个整数：");
             int one = input.nextInt();
             System.out.print("请输入第二个整数：");
             int two = input.nextInt();
             return one/two;
-        }catch(ArithmeticException e){
-            System.out.println("除数不允许为0~");
-            return 0;
-        }finally{
-            System.out.println("=====运算结束=====");
-            input.close();
-        }
+
     }
 
 }
