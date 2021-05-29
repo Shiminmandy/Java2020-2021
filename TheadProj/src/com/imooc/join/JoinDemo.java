cc0e2472a729f73f56d670788d9b2fd470a4a8f2
Shiminmandy <mandyshimin18@gmail.com>
2021-05-28 23:48:21 -0400

    join

---
+++ TheadProj/src/com/imooc/join/JoinDemo.java
@@ -0,0 +1,24 @@
+package com.imooc.join;
+class MyThread extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++) {
+            System.out.println(getName() + "正在执行！" + i + "次");
+        }
+    }
+}
+public class JoinDemo {
+    public static void main(String[] args) {
+        MyThread mt=new MyThread();
+        mt.start();
+        //join方法抢占资源:等待调用该方法的线程结束后才能执行
+        try {
+            mt.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for(int i=1;i<=10;i++){
+            System.out.println("主线程运行第"+i+"次");
+        }
+        System.out.println("主线程运行结束！");
+    }
+}
