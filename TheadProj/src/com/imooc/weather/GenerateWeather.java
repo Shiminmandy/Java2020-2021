a0492bd1b43ed772188dedf857574062f8985d21
Shiminmandy <mandyshimin18@gmail.com>
2021-06-03 15:19:18 -0400

    ThreadWeather

--- TheadProj/src/com/imooc/weather/GenerateWeather.java
+++ TheadProj/src/com/imooc/weather/GenerateWeather.java
@@ -7,7 +7,7 @@ public class GenerateWeather implements Runnable{
     }
     @Override
     public void run() {
-        for(int i=0;i<100;i++);{
+        for(int i=0;i<100;i++){
             weather.generate();
             try {
                 Thread.sleep(1000);
